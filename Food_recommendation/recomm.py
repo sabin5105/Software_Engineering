89b91f036b6ceb32418df1099fa87780cf2960bb
Sabin <50198431+sabin5105@users.noreply.github.com>
2022-06-30 17:46:36 +0900

    Update recomm.py

--- Food_recommendation/recomm.py
+++ Food_recommendation/recomm.py
@@ -1,4 +1,5 @@
-from curses import raw
+import re
+
 import engine
 
 
@@ -6,25 +7,43 @@ def main():
     print("Welcome to the Food Recommendation System!")
     df = engine.load_data()
 
+    # TODO: read from file
+    recents = []
     while True:
-        raw_input = input("Enter your preferences (hot oily sweet): ")
-        prefs = parse_raw_input(raw_input)
-        foods = engine.recommend_foods(df, prefs, 3)
-        print(describe_recomm(foods))
+        try:
+            raw_input = input("Enter your preferences (hot oily sweet): ")
+            prefs = parse_raw_input(raw_input)
+            foods, recents = engine.recommend_foods(df, prefs, 3, recents)
+            print(describe_recomm(foods))
+        except ValueError as e:
+            print(e)
+        except EOFError:
+            print("Bye!")
+            break
+
         print()
 
 
 def parse_raw_input(raw_input):
-    return [_clamp(float(token), 0.0, 1.0) for token in raw_input.split()]    # clamping
+    if not raw_input:
+        raise ValueError('Please enter something')
 
-
-def _clamp(x, lbound, ubound):
-    return max(min(x, ubound), lbound)
+    try:
+        return [
+            _clamp(float(token), 0.0, 1.0)
+            for token in re.split(r'\s|,\s?', raw_input)
+        ]
+    except ValueError as e:
+        raise ValueError('Please enter real number such as 0.5') from e
 
 
 def describe_recomm(foods):
     recomm_foods = ', '.join(foods.index.tolist())
     return f"Recommendations: {recomm_foods}"
+
+
+def _clamp(x, lbound, ubound):
+    return min(max(x, lbound), ubound)
 
 
 if __name__ == '__main__':
